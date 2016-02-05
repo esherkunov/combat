214756d8d9d7318067217000565c6e7eb6be709e
Evgeny Sherkunov <esherkunov@gmail.com>
2016-02-05 17:21:09 +0300

    News.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @products = Product.all
+    @news = News.order(id: :desc).limit(3)
   end
 
   def contacts
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'contacts' => 'pages#contacts'
 
   resources :products, only: [:index, :show]
+  resources :news, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
