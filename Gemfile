14b473a8d6520466fef27b28a37682dd6bd1fcc3
Ivan Bystrov <infernokernel@gmail.com>
2016-02-06 11:39:26 +0300

    add import csv

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'carrierwave'
+
+gem "active_admin_import" , github: "activeadmin-plugins/active_admin_import"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Product do
   permit_params :title, :description, :price, :product_category_id, :product_photo_path
+  active_admin_import 
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
